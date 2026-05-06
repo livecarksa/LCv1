e31378235b4236de42166247403b87a51a30da73
livecarksa <livecar.ksa@gmail.com>
2026-05-06 13:27:51 +0300

    Add SupabaseTables class with table constants

---
+++ livecar_workshop/lib/core/constants/supabase_tables.dart
@@ -0,0 +1,16 @@
+class SupabaseTables {
+  SupabaseTables._();
+  static const String users = 'users';
+  static const String workshops = 'workshops';
+  static const String vehicles = 'vehicles';
+  static const String orders = 'orders';
+  static const String orderItems = 'order_items';
+  static const String services = 'services';
+  static const String reviews = 'reviews';
+  static const String notifications = 'notifications';
+  static const String payments = 'payments';
+  static const String aiDiagnostics = 'ai_diagnostics';
+  static const String partnerProducts = 'partner_products';
+  static const String workshopStaff = 'workshop_staff';
+  static const String maintenanceLogs = 'maintenance_logs';
+}

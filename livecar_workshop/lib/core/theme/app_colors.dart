44bc54b84cc751e02667fddac4764fadcee26fdd
livecarksa <livecar.ksa@gmail.com>
2026-05-06 13:22:53 +0300

    Create app_colors.dart

---
+++ livecar_workshop/lib/core/theme/app_colors.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  AppColors._();
+
+  static const Color bluePrimary = Color(0xFF1A4FBE);
+  static const Color blueDark    = Color(0xFF0D2A6B);
+  static const Color blueMid     = Color(0xFF1A3A8F);
+  static const Color blueSoft    = Color(0xFF2A5FD4);
+  static const Color orange      = Color(0xFFFF6B1A);
+  static const Color orangeGlow  = Color(0x26FF6B1A);
+  static const Color white       = Color(0xFFFFFFFF);
+  static const Color grayLight   = Color(0xFFF4F6FA);
+  static const Color grayMid     = Color(0xFFE8ECF4);
+  static const Color grayText    = Color(0xFF8A95B0);
+  static const Color textDark    = Color(0xFF0D1A3A);
+  static const Color textMid     = Color(0xFF3A4A6B);
+  static const Color green       = Color(0xFF00C896);
+  static const Color greenSoft   = Color(0x1F00C896);
+  static const Color red         = Color(0xFFFF3B5C);
+  static const Color redSoft     = Color(0x1AFF3B5C);
+  static const Color yellow      = Color(0xFFFFB800);
+  static const Color yellowSoft  = Color(0x1FFFB800);
+}
